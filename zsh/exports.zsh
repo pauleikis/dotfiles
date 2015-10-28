29cee9416c2ba70526e15d17383b89316d748844
pauleikis <tsiongas@gmail.com>
2015-10-28 10:54:04 +0200

    added haskell to PATH

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,1 +1,7 @@
 [[ -x /usr/libexec/java_home ]] && export JAVA_HOME=$(/usr/libexec/java_home)
+
+# Add GHC 7.10.2 to the PATH, via https://ghcformacosx.github.io/
+export GHC_DOT_APP="/Applications/ghc-7.10.2.app"
+if [ -d "$GHC_DOT_APP" ]; then
+  export PATH="${HOME}/.local/bin:${HOME}/.cabal/bin:${GHC_DOT_APP}/Contents/bin:${PATH}"
+fi
